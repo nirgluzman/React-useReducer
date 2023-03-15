b1c8449c318f35bbc02ef971b82b58599903a806
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 00:43:31 +0100

    sample counter app with useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Hello World</div>;
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </>
+  );
 }
 
 export default App;
